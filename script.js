5273e3e41a9dc925696fa768f809679f5ddeb499
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2025-09-19 21:14:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
-  
+	e.preventDefault();
+	let r=parseFloat(document.getElementById("radius").value);
+	if(r<=0 || isNaN(r)){
+		alert("Please enter a valid positive number for radius.");
+		return;
+	}
+	let volume=(4/3)*(Math.PI)*(r*r*r);
+	document.getElementById("volume").value=volume.toFixed(4)
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
